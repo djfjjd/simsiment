9961573dd04b6c67ec4985b40299313871546246
djfjjd <djfjjd@djfjjdui-MacBookAir.local>
2026-07-26 19:12:28 +0900

    Render homepage fully on the server

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
-import HomeClient from "./HomeClient";
+import HomeView from "./HomeView";
+
+export const dynamic = "force-dynamic";
 
 export default function Home() {
-  return <HomeClient />;
+  return <HomeView />;
 }
